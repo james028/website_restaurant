114947826c3bcd7cde490f017b48044f01864d5a
Jakub Kania <kubak.15@gmail.com>
2017-03-23 14:52:38 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,37 @@
+//slider superslider
+
+$(document).ready(function() {
+  $('#slides').superslides({
+    slide_easing: 'easeInOutCubic',
+    slide_speed: 800,
+    pagination: true,
+    hashchange: true,
+    scrollable: true
+  });
+});
+
+
+
+
+//Rozwijane meny z uzyciem jQuery
+
+$(function() {
+	menu = $('nav ul');
+
+  $('#openup').on('click', function(e) {
+    e.preventDefault(); menu.slideToggle(); //funkcja rozwijajaca menu
+  });
+  
+  $(window).resize(function(){
+    var w = $(this).width(); if(w > 700 && menu.is(':hidden')) {
+      menu.removeAttr('style'); // Jesli szerokosc jest wieksza niz 700px zostaje usuniety atrybut style
+    }
+  });
+  
+  $('nav li').on('click', function(e) {                
+    var w = $(window).width(); if(w < 700 ) {
+      menu.slideToggle();  // Jesli szerokosc jest mniejsza niz 700px to menu sie rozwija i zwija
+    }
+  });
+  $('.open-menu').height($(window).height());
+});
